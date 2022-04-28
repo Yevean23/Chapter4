cf96e18714ba201df035e565494485a1f870519c
TrophyAI <94488941+TrophyAI@users.noreply.github.com>
2022-04-28 19:15:25 -0400

    first problem set in 4, done

---
+++ 4.1.2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/*
+Exercises Section 4.1.2
+Exercise 4.1: What is the value returned by 5 + 10 * 20/2?
+Exercise 4.2: Using Table 4.12 (p. 166), parenthesize the following
+expressions to indicate the order in which the operands are grouped:
+(a) * vec.begin()
+(b) * vec.begin() + 1
+*/
+
+int main() {
+
+	// 4.1 105
+	std::cout << (5 + 10 * 20 / 2) << std::endl;
+
+	// 4.2
+	// (a) *(vec.begin())
+	// (b) (*(vec.begin()) + 1)
+
+
+	return 0;
+}
