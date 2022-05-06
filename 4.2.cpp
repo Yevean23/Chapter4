2d544cba96abb8d6b19fc1827fc0f94259613321
Yevean23 <vovk.yev@gmail.com>
2022-05-05 22:46:04 -0400

    adding 4.2

---
+++ 4.2.cpp
@@ -0,0 +1,24 @@
+/*
+Exercises Section 4.2
+Exercise 4.4: Parenthesize the following expression to show how it is
+evaluated. Test your answer by compiling the expression (without
+parentheses) and printing its result.
+12 / 3 * 4 + 5 * 15 + 24 % 4 / 2
+Exercise 4.5: Determine the result of the following expressions.
+(a) -30 * 3 + 21 / 5
+(b) -30 + 3 * 21 / 5
+(c) 30 / 3 * 21 % 5
+(d) -30 / 3 * 21 % 4
+Exercise 4.6: Write an expression to determine whether an int value is
+even or odd.
+Exercise 4.7: What does overflow mean? Show three expressions that will
+overflow.
+*/
+
+
+int main() {
+	// 4.4
+
+
+	return 0;
+}
